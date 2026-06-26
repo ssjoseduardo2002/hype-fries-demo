50ab8671ed02ef05914d2e0af7f88745fa5269eb
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-06-26 16:05:25 -0600

    importación de la galería

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,23 @@ import {NavbarComponent} from './components/navbar/navbar.component';
 import {HeroComponent} from './components/hero/hero.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {SocialMatricsComponent} from './components/social-matrics/social-matrics.component';
+import {InstagramGalleryComponent} from './components/instagram-gallery/instagram-gallery'; // Importación añadida
 import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, MenuComponent, SocialMatricsComponent, FooterComponent],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent, 
+    HeroComponent, 
+    MenuComponent, 
+    SocialMatricsComponent, 
+    InstagramGalleryComponent, // Componente registrado
+    FooterComponent
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'], // Nota: Cambié a styleUrls por convención, ajusta si es styleUrl
 })
 export class App {
   protected readonly title = signal('Hype Fries');
