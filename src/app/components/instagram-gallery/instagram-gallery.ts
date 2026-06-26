01930b7523a1cc551fa561e53a39e690d6c75a42
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-06-26 15:59:02 -0600

    Asignación de publicaciones de HYPE al instagram-gallery.ts

--- src/app/components/instagram-gallery/instagram-gallery.ts
+++ src/app/components/instagram-gallery/instagram-gallery.ts
@@ -11,11 +11,11 @@ import { CommonModule } from '@angular/common'; // Asegúrate de tenerlo si es u
 export class InstagramGalleryComponent {
   // Arreglo con la información de los posts de HYPE FRIES
   instagramPosts = [
-    { id: 1, image: 'assets/images/gallery/1.jpg', link: 'https://www.instagram.com/p/tu-post-1' },
-    { id: 2, image: 'assets/images/gallery/2_2.jpg', link: 'https://www.instagram.com/p/tu-post-2' },
-    { id: 3, image: 'assets/images/gallery/3_2.jpg', link: 'https://www.instagram.com/p/tu-post-3' },
-    { id: 4, image: 'assets/images/gallery/4_2.jpg', link: 'https://www.instagram.com/p/tu-post-4' },
-    { id: 5, image: 'assets/images/gallery/5_2.jpg', link: 'https://www.instagram.com/p/tu-post-5' },
-    { id: 6, image: 'assets/images/gallery/6_2.jpg', link: 'https://www.instagram.com/p/tu-post-6' }
+    { id: 1, image: 'assets/images/gallery/1.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=3' },
+    { id: 2, image: 'assets/images/gallery/2_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=4' },
+    { id: 3, image: 'assets/images/gallery/3_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=5' },
+    { id: 4, image: 'assets/images/gallery/4_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=2' },
+    { id: 5, image: 'assets/images/gallery/5_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=1' },
+    { id: 6, image: 'assets/images/gallery/6_2.jpg', link: 'https://www.instagram.com/p/DXz_nUwmyBj/?img_index=5' }
   ];
 }
