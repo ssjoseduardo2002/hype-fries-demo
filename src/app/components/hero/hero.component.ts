a940204d00d24c31a75918af7ed529dbbe9cb3f3
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-07-08 22:06:26 -0600

    navbar.scss y componente hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
+  standalone: true, // Aseguramos que sea standalone
   imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  // Aquí podemos agregar lógica en el futuro, como un título dinámico,
+  // o el control de un video que se pause al hacer scroll.
+  // Por ahora, mantenemos la estructura lista para recibir el contenido visual.
 }
