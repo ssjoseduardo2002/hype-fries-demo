1d9921aad4561f752b072e6384b73f9ebfe89914
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-07-08 21:56:20 -0600

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Necesario para usar [ngClass]
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true, // Asegúrate que esté marcado como standalone
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  // Escucha el evento de scroll en la ventana
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Si el usuario baja más de 50px, activamos el estado "scrolled"
+    this.isScrolled = window.scrollY > 50;
+  }
 }
