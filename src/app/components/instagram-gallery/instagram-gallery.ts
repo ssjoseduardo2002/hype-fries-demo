b688fc38bef2d7c9ee426e6331b14299635f0869
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-07-08 21:37:41 -0600

    visualización de imágenes

--- src/app/components/instagram-gallery/instagram-gallery.ts
+++ src/app/components/instagram-gallery/instagram-gallery.ts
@@ -10,12 +10,12 @@ import { CommonModule } from '@angular/common'; // Asegúrate de tenerlo si es u
 })
 export class InstagramGalleryComponent {
   // Arreglo con la información de los posts de HYPE FRIES
-  instagramPosts = [
-    { id: 1, image: 'assets/images/gallery/1.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=3' },
-    { id: 2, image: 'assets/images/gallery/2_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=4' },
-    { id: 3, image: 'assets/images/gallery/3_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=5' },
-    { id: 4, image: 'assets/images/gallery/4_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=2' },
-    { id: 5, image: 'assets/images/gallery/5_2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=1' },
-    { id: 6, image: 'assets/images/gallery/6_2.jpg', link: 'https://www.instagram.com/p/DXz_nUwmyBj/?img_index=5' }
-  ];
+instagramPosts = [
+  { id: 1, image: 'assets/images/gallery/1.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=3' },
+  { id: 2, image: 'assets/images/gallery/2.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=4' },
+  { id: 3, image: 'assets/images/gallery/3.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=5' },
+  { id: 4, image: 'assets/images/gallery/4.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=2' },
+  { id: 5, image: 'assets/images/gallery/5.jpg', link: 'https://www.instagram.com/p/DZfzdrwD3vF/?img_index=1' },
+  { id: 6, image: 'assets/images/gallery/6.jpg', link: 'https://www.instagram.com/p/DXz_nUwmyBj/?img_index=5' }
+];
 }
