22123864c8526050d73073415614b890045410fe
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-07-11 13:27:45 -0600

    Tarea 1 día 8

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -19,19 +19,18 @@ export class MenuComponent {
     { id: 4, titulo: 'Malteadas y Limonada', imagen: 'assets/images/gallery/44.jpg' },
   ];
 
-  // Base de datos completa
   platillos: Platillo[] = [
     // --- Hamburguesas (Categoría 1) ---
-    { id: 101, categoriaId: 1, nombre: 'Hamburguesa Clásica', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, lechuga, tomate, jalapeños, cebolla, cátsup y ranch.', precio: 176.00 },
-    { id: 102, categoriaId: 1, nombre: 'Combo Clásico', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, lechuga, tomate, jalapeños, cebolla, cátsup y ranch. Papas grandes, refresco o limonada.', precio: 293.00 },
-    { id: 103, categoriaId: 1, nombre: 'Hamburguesa Hawaiana', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, piña, lechuga, tomate, jalapeños, cebolla carameliza, BBQ y ranch.', precio: 189.00 },
-    { id: 104, categoriaId: 1, nombre: 'Combo Hawaiana', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, piña, lechuga, tomate, jalapeños, cebolla carameliza, BBQ y ranch. Incluye papas grandes, refresco o limonada.', precio: 306.00 },
-    { id: 105, categoriaId: 1, nombre: 'Hamburguesa de Champiñones', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, champiñones con queso, lechuga, tomate, jalapeño, cebolla, cátsup y ranch.', precio: 189.00 },
-    { id: 106, categoriaId: 1, nombre: 'Combo Champiñones', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, champiñones con queso, lechuga, tomate, jalapeño, cebolla, cátsup y ranch. Incluye papas grandes, refresco o limonada.', precio: 257.00 },
-    { id: 107, categoriaId: 1, nombre: 'Hamburguesa Smash', descripcion: 'Doble porcion de 130 gr. carne de res fresca, queso amarillo, pan brioche, tocino, cebolla carameliza, aderezo SMASH, pepinillos.', precio: 209.00 },
-    { id: 108, categoriaId: 1, nombre: 'Combo Smash', descripcion: 'Dos porciones de 130 gr. carne de res fresca, queso amarillo, pan brioche, tocino, cebolla carameliza, aderezo SMASH, pepinillos. Incluye papas grandes, refresco o limonada.', precio: 336.00 },
-    { id: 109, categoriaId: 1, nombre: 'Hamburguesa Chicken HYPE', descripcion: 'Pechuga de pollo empanizada en salsa de tu eleccion (BBQ, buffalo, Mango habanero), pan brioche, vegetales y ranch.', precio: 176.00 },
-    { id: 110, categoriaId: 1, nombre: 'Combo Chicken HYPE', descripcion: 'Pechuga de pollo empanizada en salsa de tu eleccion (BBQ, buffalo, Mango habanero), pan brioche, vegetales y ranch. Papas grandes, refresco o limonada.', precio: 293.00 },
+    { id: 101, categoriaId: 1, nombre: 'Hamburguesa Clásica', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, lechuga, tomate, jalapeños, cebolla, cátsup y ranch.', precio: 176.00, imagen: 'assets/images/gallery/clasica.jpg' },
+    { id: 102, categoriaId: 1, nombre: 'Combo Clásico', descripcion: 'Incluye papas grandes, refresco o limonada.', precio: 293.00, imagen: 'assets/images/gallery/clasica.jpg' },
+    { id: 103, categoriaId: 1, nombre: 'Hamburguesa Hawaiana', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, piña, lechuga, tomate, jalapeños, cebolla carameliza, BBQ y ranch.', precio: 189.00, imagen: 'assets/images/gallery/hawaiana.jpg' },
+    { id: 104, categoriaId: 1, nombre: 'Combo Hawaiana', descripcion: 'Incluye papas grandes, refresco o limonada.', precio: 306.00, imagen: 'assets/images/gallery/hawaiana.jpg' },
+    { id: 105, categoriaId: 1, nombre: 'Hamburguesa de Champiñones', descripcion: '130 gr. de fresca carne de res, queso gratinado, pan costra de queso, tocino, champiñones con queso, lechuga, tomate, jalapeño, cebolla, cátsup y ranch.', precio: 189.00, imagen: 'assets/images/gallery/champiñones.jpg' },
+    { id: 106, categoriaId: 1, nombre: 'Combo Champiñones', descripcion: 'Incluye papas grandes, refresco o limonada.', precio: 257.00, imagen: 'assets/images/gallery/champiñones.jpg' },
+    { id: 107, categoriaId: 1, nombre: 'Hamburguesa Smash', descripcion: 'Doble porcion de 130 gr. carne de res fresca, queso amarillo, pan brioche, tocino, cebolla carameliza, aderezo SMASH, pepinillos.', precio: 209.00, imagen: 'assets/images/gallery/6.jpg' },
+    { id: 108, categoriaId: 1, nombre: 'Combo Smash', descripcion: 'Incluye papas grandes, refresco o limonada.', precio: 336.00, imagen: 'assets/images/gallery/6.jpg' },
+    { id: 109, categoriaId: 1, nombre: 'Hamburguesa Chicken HYPE', descripcion: 'Pechuga de pollo empanizada en salsa de tu eleccion.', precio: 176.00, imagen: 'assets/images/gallery/7.jpg' },
+    { id: 110, categoriaId: 1, nombre: 'Combo Chicken HYPE', descripcion: 'Incluye papas grandes, refresco o limonada.', precio: 293.00, imagen: 'assets/images/gallery/7.jpg' },
 
     // --- Papas (Categoría 2) ---
     { id: 301, categoriaId: 2, nombre: 'Papas Superhype', descripcion: 'Aderezos a elección y dos toppings.', precio: 130.00 },
@@ -39,13 +38,13 @@ export class MenuComponent {
     { id: 303, categoriaId: 2, nombre: 'Papas Hype', descripcion: 'Aderezos a elección y un topping.', precio: 150.00 },
 
     // --- Boneless (Categoría 3) ---
-    { id: 201, categoriaId: 3, nombre: 'Combo Boneless BBQ', descripcion: 'Orden con 8 a 9 piezas de boneless con papas grandes, refresco o limonada.', precio: 293.00 },
+    { id: 201, categoriaId: 3, nombre: 'Combo Boneless BBQ', descripcion: 'Orden con papas grandes, refresco o limonada.', precio: 293.00, imagen: 'assets/images/gallery/papas clasicas con boneless.jpg' },
     { id: 202, categoriaId: 3, nombre: 'Boneless Buffalo', descripcion: 'Orden con 8 a 9 piezas de boneless.', precio: 176.00 },
     { id: 203, categoriaId: 3, nombre: 'Boneless BBQ', descripcion: 'Orden con 8 a 9 piezas de boneless.', precio: 176.00 },
-    { id: 204, categoriaId: 3, nombre: 'Combo Boneless Buffalo', descripcion: 'Orden con 8 a 9 piezas de boneless, con papas grandes, refresco o limonada.', precio: 293.00 },
+    { id: 204, categoriaId: 3, nombre: 'Combo Boneless Buffalo', descripcion: 'Orden con papas grandes, refresco o limonada.', precio: 293.00, imagen: 'assets/images/gallery/papas clasicas con boneless.jpg' },
     { id: 205, categoriaId: 3, nombre: 'Boneless Pimienta Limón', descripcion: 'Orden con 8 a 9 piezas de boneless.', precio: 156.00 },
-    { id: 206, categoriaId: 3, nombre: 'Combo Boneless Pimienta Limón', descripcion: 'Orden con 8 a 9 piezas de boneless, con papas grandes, refresco o limonada.', precio: 257.00 },
-    { id: 207, categoriaId: 3, nombre: 'Combo Boneless Mango Habanero', descripcion: 'Orden con 8 a 9 piezas de boneless con papas grandes, refresco o limonada.', precio: 293.00 },
+    { id: 206, categoriaId: 3, nombre: 'Combo Boneless Pimienta Limón', descripcion: 'Orden con papas grandes, refresco o limonada.', precio: 257.00, imagen: 'assets/images/gallery/papas clasicas con boneless.jpg' },
+    { id: 207, categoriaId: 3, nombre: 'Combo Boneless Mango Habanero', descripcion: 'Orden con papas grandes, refresco o limonada.', precio: 293.00, imagen: 'assets/images/gallery/papas clasicas con boneless.jpg' },
     { id: 208, categoriaId: 3, nombre: 'Boneless Mango Habanero', descripcion: 'Orden con 8 a 9 piezas de boneless.', precio: 176.00 },
 
     // --- Malteadas y Bebidas (Categoría 4) ---
@@ -56,7 +55,7 @@ export class MenuComponent {
     { id: 405, categoriaId: 4, nombre: 'Fanta', descripcion: '355 ml.', precio: 32.00 },
     { id: 406, categoriaId: 4, nombre: 'Fresca', descripcion: '355 ml.', precio: 46.00 },
     { id: 407, categoriaId: 4, nombre: 'Sidral Mundet', descripcion: '355 ml.', precio: 46.00 },
-    { id: 408, categoriaId: 4, nombre: 'Limonada Rosa', descripcion: '16 Onzas.', precio: 39.00 },
+    { id: 408, categoriaId: 4, nombre: 'Limonada Rosa', descripcion: '16 Onzas.', precio: 39.00, imagen: 'assets/images/gallery/para limonada rosa.jpg' },
     { id: 409, categoriaId: 4, nombre: 'Coca Cola Regular', descripcion: '355 ml.', precio: 46.00 },
     { id: 410, categoriaId: 4, nombre: 'Sprite 355 ml', descripcion: 'Botella de plastico', precio: 25.00 }
   ];
--- src/app/components/menu/menu.model.ts
+++ src/app/components/menu/menu.model.ts
@@ -1,13 +1,16 @@
+// Define la estructura de las categorías principales del menú
 export interface Categoria {
   id: number;
   titulo: string;
-  imagen: string;
+  imagen: string; // Imagen representativa de la categoría
 }
 
+// Define la estructura de cada producto, ahora con soporte para imagen opcional
 export interface Platillo {
   id: number;
-  categoriaId: number; // Esto debe coincidir con el 'id' de la Categoria
+  categoriaId: number; // Relación con Categoria.id
   nombre: string;
   descripcion: string;
   precio: number;
+  imagen?: string; // Propiedad opcional: si el platillo tiene foto, la ponemos; si no, queda vacía
 }
