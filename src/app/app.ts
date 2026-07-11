3fe9cb7da662bd4a192a57826cfc3296dc0304a6
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-07-10 19:15:17 -0600

    Tarea 1 día 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import {Component, signal} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {HeroComponent} from './components/hero/hero.component';
-import {MenuComponent} from './components/menu/menu.component';
-import {SocialMatricsComponent} from './components/social-matrics/social-matrics.component';
-import {InstagramGalleryComponent} from './components/instagram-gallery/instagram-gallery'; // Importación añadida
-import {FooterComponent} from './components/footer/footer.component';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SocialMatricsComponent } from './components/social-matrics/social-matrics.component';
+import { InstagramGalleryComponent } from './components/instagram-gallery/instagram-gallery';
+import { FooterComponent } from './components/footer/footer.component';
+// Importación necesaria para el botón flotante
+import { WhatsappButtonComponent } from './components/whatsapp-button/whatsapp-button'; 
 
 @Component({
   selector: 'app-root',
@@ -16,11 +18,12 @@ import {FooterComponent} from './components/footer/footer.component';
     HeroComponent, 
     MenuComponent, 
     SocialMatricsComponent, 
-    InstagramGalleryComponent, // Componente registrado
-    FooterComponent
+    InstagramGalleryComponent, 
+    FooterComponent,
+    WhatsappButtonComponent // Componente registrado aquí
   ],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss'], // Nota: Cambié a styleUrls por convención, ajusta si es styleUrl
+  styleUrl: './app.scss', // Ajustado a styleUrl (estándar Angular 17+)
 })
 export class App {
   protected readonly title = signal('Hype Fries');
